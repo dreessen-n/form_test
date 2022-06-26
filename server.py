623c6c857dce7e731f9f6f16f166e0a81fd8e5c2
dreessen-n <dreessen.edu@gmail.com>
2022-06-25 22:27:19 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    # Never render a template on a POST request.
+    # Instead we will redirect to our index route.
+    return redirect('/')
+
+# Error message for 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return f'Sorry! No response. Try again.'
+
+# Ensure file is run directly and not from different
+# module, and run localhost on port 5001 for mac
+if __name__=="__main__":
+    app.run(host='localhost', port=5001, debug=True)
